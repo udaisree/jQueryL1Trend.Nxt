8ca385371a0f13280f9c9a2dccdf1952168bc3bc
udaisree <udaisreeabraham@gmail.com>
2018-11-15 06:18:06 -0800

    Add files via upload

---
+++ Gif.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace Progress_Bar_2._0
+{
+	public partial class Gif : Form
+	{
+		public Gif()
+		{
+			InitializeComponent();
+		}
+
+		private void Gif_Load(object sender, EventArgs e)
+		{
+			pictureBox1.Visible = true;
+			pictureBox1.Dock = DockStyle.Fill;
+			backgroundWorker1.WorkerReportsProgress = true;
+			backgroundWorker1.RunWorkerAsync();
+		}
+		private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+		{
+			Thread.Sleep(4000);
+		}
+		private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			this.Close();
+		}
+
+
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progress_Bar_2._0
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Gif Obj = new Gif();
+			Obj.ShowDialog();
+			Obj.Dispose();
+			
+		}
+	}
+}
